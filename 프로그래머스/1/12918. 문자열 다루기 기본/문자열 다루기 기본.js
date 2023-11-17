459a2567715257fafd71c8c0cd5cf3c790b4fcd0
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2023-11-17 09:48:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = true;
+    let arr = [];
+    
+    arr = s.split('');
+    
+    if (arr.length !== 4 && arr.length !== 6) answer = false;
+    arr.forEach(e => { 
+        if (e < '0' || e > '9') answer = false;
+    });
+    
+    return answer;
+}
