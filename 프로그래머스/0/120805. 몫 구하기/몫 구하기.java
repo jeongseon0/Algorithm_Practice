ec40d805da75890aa0cfd89970f38686c928a040
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-05-22 17:57:30 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 / num2; // 몫
+        //int answer2 = num1 % num2; // 나머지
+        return answer;
+    }
+}
