a956d74bf24662e761c823b06bf3b7a65fb7afb5
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-10-10 15:28:17 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,10 @@
+function solution(price) {
+    let answer = 0;
+    
+    if (price >= 500000) answer = Math.trunc(price * 0.8);
+    else if (price >= 300000) answer = Math.trunc(price * 0.9);
+    else if (price >= 100000) answer = Math.trunc(price * 0.95);
+    else answer = price;
+    
+    return answer;
+}
