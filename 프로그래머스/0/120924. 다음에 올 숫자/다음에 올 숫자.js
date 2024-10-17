5684a04dee34362ce8debcbf694b8066d9f2643d
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-10-17 18:35:17 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,10 @@
+function solution(common) {
+    let answer = 0;
+    let d = common[1] - common[0];
+    let r = common[1] / common[0];
+    
+    if(common[2] - common[1] === d) answer = common[common.length-1] + d;
+    else answer = common[common.length-1] * r;
+    
+    return answer;
+}
