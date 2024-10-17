35a070c5c4fa44870b4e4927714e781cf0cd054e
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-10-17 20:05:07 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,18 +1,21 @@
 function solution(num, total) {
     let answer = [];
     
-    answer[0] = 0;
-    for(let i = 1; i < num; i++) {
-        answer[i] = answer[i-1] + 1;
-    }
+    // answer[0] = 0;
+    // for(let i = 1; i < num; i++) answer[i] = answer[i-1] + 1;
     
-    while(1) {
-        let sum = 0;
-        for(let i = 0; i < num; i++) sum += answer[i];
-        if(sum === total) break;
-        if(sum < total) for(let i = 0; i < num; i++) answer[i]++;
-        else for(let i = 0; i < num; i++) answer[i]--;
-    }
+    // while(1) {
+    //     let sum = 0;
+    //     for(let i = 0; i < num; i++) sum += answer[i];
+    //     if(sum === total) break;
+    //     if(sum < total) for(let i = 0; i < num; i++) answer[i]++;
+    //     else for(let i = 0; i < num; i++) answer[i]--;
+    // }
+    
+    answer[0] = total / num - (num - 1) / 2;
+    
+    for(let i = 1; i < num; i++) answer[i] = answer[i-1] + 1;
+    
     
     return answer;
 }
