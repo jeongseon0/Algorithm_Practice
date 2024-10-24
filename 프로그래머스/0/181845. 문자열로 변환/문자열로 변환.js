230e4f5111f14fa3789e9b231c65692cd6763ff3
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-10-24 15:17:28 +0900

    [level 0] Title: 문자열로 변환, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
@@ -1,22 +1,15 @@
 function solution(n) {
     let answer = '';
-    let index = 10000;
+    let temp = [];
     
-    answer = n.toString();
+    // answer = n.toString();
     
-//     while(n > 10) {
-//         console.log(n, index, answer);
-//         if(Math.trunc(n / index) === 0) {
-//             index /= 10;
-//             continue;
-//         }
-//         answer += Math.trunc(n / index).toString();
-//         n = n % index;
-//         console.log(n, index, answer);
-//     }
+    while(n > 0) {        
+        temp.unshift((n%10).toString());
+        n = Math.trunc(n/10);
+    }
     
-//     if(n > 0) answer += n.toString();
-//     console.log(n, index, answer);
+    answer = temp.join('');
     
     return answer;
 }
