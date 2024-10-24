ae331b1451e8a8ee8bf181dc879ff722a0e145b9
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-10-24 14:54:36 +0900

    [level 0] Title: 부분 문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/README.md
+++ 프로그래머스/0/181842. 부분 문자열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 부분 문자열 - 181842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181842#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 14:40:24
+2024년 10월 24일 14:54:35
 
 ### 문제 설명
 
--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.js
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.js
@@ -1,7 +1,17 @@
 function solution(str1, str2) {
     let answer = 0;
     
-    if(str2.includes(str1)) answer = 1;
+    // if(str2.includes(str1)) answer = 1;
+    
+    for(let i = 0; i <= str2.length - str1.length; i++) {
+        if(str2[i] === str1[0]) {
+            let j;
+            for(j = 1; j < str1.length; j++) {
+                if(str2[i+j] !== str1[j]) break;
+            }
+            if(j === str1.length) answer = 1;
+        }
+    }
     
     return answer;
 }
