49ba2b4dccea00ee09989ec2eb946d246b3cbc2b
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-10-31 15:33:45 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.js
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.js
@@ -1,7 +1,21 @@
 function solution(my_string) {
     let answer = [];
     
-    answer = my_string.split(' ');
+    // answer = my_string.split(' ');
+    
+    let n = 0;
+    let temp = '';
+    
+    for(let i = 0; i < my_string.length; i++) {
+        if(my_string[i] !== ' ') temp += my_string[i];
+        else {
+            answer[n] = temp;
+            n++;
+            temp = '';
+        }
+    }
+    
+    if(temp.length) answer[n] = temp;
     
     return answer;
 }
