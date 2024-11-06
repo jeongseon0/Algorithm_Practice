48aeba9e303ade12b6838c79353eeb41f3ee2206
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-11-06 13:44:48 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -1,6 +1,8 @@
 function solution(my_string, n) {
     let answer = '';
     
+    // answer = my_string.slice(my_string.length-n, my_string.length);
+    
     for(let i = my_string.length-n; i < my_string.length; i++) {
         answer += my_string[i];
     }
